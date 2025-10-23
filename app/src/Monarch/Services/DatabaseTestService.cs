738a8a3ef6ddff3548d294645ea42065108a1b11
bsqua <bsquared1504@gmail.com>
2025-10-23 09:52:40 -0500

    Finished test app

--- app/src/Monarch/Services/DatabaseTestService.cs
+++ app/src/Monarch/Services/DatabaseTestService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.IO;
 using Microsoft.Data.SqlClient;
+using Monarch.Models;
 
 namespace Monarch.Services
 {
@@ -12,43 +13,29 @@ namespace Monarch.Services
 
         public DatabaseTestService()
         {
-            string password = File.ReadAllText("/run/secrets/monarch_sql_monarch_password").Trim();
+            string password = File.ReadAllText("/run/secrets/monarch_sql_monarch_password");
             _connectionString = $"Server=sqlserver;Database=monapi;User ID=monarch;Password={password};TrustServerCertificate=True;";;
         }
 
-        public string TestConnection()
+        public List<ResourceStatus> GetAllStatuses()
         {
-            try
-            {
-                using var connection = new SqlConnection(_connectionString);
-                connection.Open();
-                return "Database connection succeeded.";
-            }
-            catch (Exception ex)
-            {
-                return $"Database connection failed: {ex.Message}";
-            }
-        }
-
-        public List<(int Id, string ResourceName, string CurrentStatus, DateTime LastUpdated)> GetAllStatuses()
-        {
-            var results = new List<(int, string, string, DateTime)>();
+            var results = new List<ResourceStatus>();
 
             using var connection = new SqlConnection(_connectionString);
             connection.Open();
 
-            string query = "SELECT id, resourceName, currentStatus, lastUpdated FROM sampleTable;";
+            string query = "SELECT resourceName, currentStatus FROM sampleTable;";
             using var command = new SqlCommand(query, connection);
             using var reader = command.ExecuteReader();
 
             while (reader.Read())
             {
-                results.Add((
-                    reader.GetInt32(0),
-                    reader.GetString(1),
-                    reader.GetString(2),
-                    reader.GetDateTime(3)
-                ));
+                results.Add(new ResourceStatus
+                    {
+                        ResourceName = reader.GetString(reader.GetOrdinal("resourceName")),
+                        CurrentStatus = reader.GetString(reader.GetOrdinal("currentStatus"))
+                    }
+                );
             }
 
             return results;
---
+++ app/src/Monarch/models/ResourceStatus.cs
@@ -0,0 +1,8 @@
+namespace Monarch.Models
+{
+    public class ResourceStatus
+    {
+        public string ResourceName { get; set; } = string.Empty;
+        public string CurrentStatus { get; set; } = string.Empty;
+    }
+}
