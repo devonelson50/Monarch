f1ef7cfb53d0e0ec06f27447056c6b0af68fdf2a
devonelson50 <devon@455garage.com>
2025-11-04 20:51:24 -0600

    GetApps in progress, evaluating RestSharp package

--- app/src/Monapi.Worker/NewRelic.cs
+++ app/src/Monapi.Worker/NewRelic.cs
@@ -1,7 +1,10 @@
 using System.Collections;
+using System.Threading.Tasks;
+using RestSharp;
 
 namespace Monapi.Worker;
 
+// https://api.newrelic.com/docs/#
 public class NewRelic
 {
     private readonly String apiKey;
@@ -9,14 +12,48 @@ public class NewRelic
     public NewRelic()
     {
         this.apiKey = File.ReadAllText("/run/secrets/monarch_newrelic_api_key");
+        this.monapiKey = File.ReadAllText("/run/secrets/monarch_sql_monapi_password");
+        var apps = GetApps();
+        WriteToDatabse(apps);
 
     }
-    public ArrayList GetApps()
+
+
+    public async Task<ArrayList> GetApps()
     {
         var apps = new ArrayList();
-        var request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create("https://api.newrelic.com/v2/applications.json");
-        request.Method = "GET";
-        request.Headers["X-Api-Key"] = this.apiKey;
+        do
+        {
+            var uri = "https://api.newrelic.com/v2/applications.json";
+            var options = new RestClientOptions(uri);
+            var client = new RestClient(options);
+            var request = new RestRequest();
+            request.AddHeader("Api-Key", this.apiKey);
+            var response = await client.ExecuteAsync(request);
 
+            if (!(response.IsSuccessful))
+            {
+                throw new Exception("API request failed.");
+            }
+            else
+            {
+                var content = response.Content;    
+            }
+
+        } while (true);
+
+
+
+
+
+
+
+
+            return apps;
+    }
+    
+    public void WriteToDatabase(ArrayList apps)
+    {
+        
     }
 }
