cc4fe9e2cdf5107165aaaad9c24ab019f829c28d
bsqua <bsquared1504@gmail.com>
2026-04-25 16:57:44 -0500

    Updated all model documentation

--- app/src/Monarch/models/FilterModel.cs
+++ app/src/Monarch/models/FilterModel.cs
@@ -1,8 +1,24 @@
+
+/*  
+  Brady Brown
+  App Filters for Monarch
+  Stores filter details for applications, added to app's FilterList (see more in AppModel.cs)
+*/
+
 namespace Monarch.Models
 {
+  /*
+  Overall model for filters
+  Includes the following:
+      -int FilterId
+      -string FilterName (nullable)
+  */
+
   public class FilterModel
   {
+    //Auto-increment id pulled directly from filters table in Monarch
     public int FilterId { get; set; }
+    //Name specified by user during config
     public string? FilterName { get; set; }
   }
 }
--- app/src/Monarch/models/NewRelicApp.cs
+++ app/src/Monarch/models/NewRelicApp.cs
@@ -1,8 +1,25 @@
+
+/*  
+  Brady Brown
+  New Relic App Details for Monarch
+  Stores New Relic config details, used primarily for inital configuration lists
+*/
+
 namespace Monarch.Models
 {
+
+  /*
+  Overall model for New Relic application
+  Includes the following:
+      -int AppId
+      -string AppName
+  */
+
   public class NewRelicApp
   {
+    //Id pulled from newRelicApps table, which is pulled from New Relic directly
     public int AppId { get; set; } = 0;
+    //Mame pulled from newRelicApps table, which is pulled from New Relic directly
     public string AppName { get; set; } = string.Empty;
   }
 }
